73d42171ad4c57423644bf7823b11efa702c8ef3
Luiz Nascimento <luns.wm@hotmail.com>
2021-10-23 21:16:25 -0300

    Sobrescrita-herança, entraada e saida de dados em java

---
+++ Java Fundamentos/input-output/input-output-um/src/main/java/Formata.java
@@ -0,0 +1,16 @@
+import java.text.DecimalFormat;
+
+public class Formata {
+
+    public static void main(String[] args) {
+
+        double valor = 2000.0;
+        double parcelas = 3.0;
+
+        double prestacao = valor/parcelas;
+
+        DecimalFormat df = new DecimalFormat("0.###");
+
+        System.out.println(df);
+    }
+}
