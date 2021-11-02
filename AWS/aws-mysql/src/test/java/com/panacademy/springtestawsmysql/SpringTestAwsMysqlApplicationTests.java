f119d487a66b2c49236b20c370e1ccfb8a5a3046
Luiz Nascimento <luns.wm@hotmail.com>
2021-11-01 21:46:07 -0300

    implementado lambda na aws

---
+++ AWS/aws-mysql/src/test/java/com/panacademy/springtestawsmysql/SpringTestAwsMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panacademy.springtestawsmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestAwsMysqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
